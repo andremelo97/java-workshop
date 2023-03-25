e22c1aae0dd5e44e62f0b122d72aaa776842a8d8
andremelo97 <andre.melo9715@gmail.com>
2023-03-25 09:38:00 -0300

    Service layer, component registration

---
+++ src/main/java/com/melo/demo/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.melo.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.melo.demo.entities.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
---
+++ src/main/java/com/melo/demo/resources/OrderResource.java
@@ -0,0 +1,34 @@
+package com.melo.demo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.melo.demo.entities.Order;
+import com.melo.demo.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+	
+	@Autowired
+	private OrderService OrderService;
+	
+	@GetMapping
+	public ResponseEntity<List<Order>> findAll() {
+		List<Order> list = OrderService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
+		Order obj = OrderService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
