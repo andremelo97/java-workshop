66b5f4f0aa0ba771d753219eb882cb3d58c38016
andremelo97 <andre.melo9715@gmail.com>
2023-03-27 15:16:32 -0300

    OrderItem

--- src/main/java/com/melo/demo/config/TestConfig.java
+++ src/main/java/com/melo/demo/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.melo.demo.entities.Category;
 import com.melo.demo.entities.Order;
+import com.melo.demo.entities.OrderItem;
 import com.melo.demo.entities.Product;
 import com.melo.demo.entities.User;
 import com.melo.demo.entities.enums.OrderStatus;
@@ -17,6 +18,7 @@ import com.melo.demo.repositories.CategoryRepository;
 import com.melo.demo.repositories.OrderRepository;
 import com.melo.demo.repositories.ProductRepository;
 import com.melo.demo.repositories.UserRepository;
+import com.melo.demo.repositories.OrderItemRepository;
 
 @Configuration
 @Profile("test")
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -41,12 +46,6 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
-		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.DELIVERED, u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1); 
-		
-		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
@@ -70,5 +69,18 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.DELIVERED, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1); 
+		
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
